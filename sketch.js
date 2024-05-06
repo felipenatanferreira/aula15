47df1f2abd77dccd9156bd77cf2618f111e5bb26
felipenatanferreira <169069012+felipenatanferreira@users.noreply.github.com>
2024-05-06 11:52:11 -0300

    Add files via upload

---
+++ sketch.js
@@ -0,0 +1,14 @@
+function setup() {
+  createCanvas(800,800);
+  background("pink")
+}
+
+function draw() {
+stroke ("blue");
+  fill("yellow");
+
+
+if (mouseIsPressed){
+rect(mouseX, mouseY, 50, 40)
+}
+}
